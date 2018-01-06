201a98d5c4ad7bc8bde2f93d7063c3715b75b5f9
Trung <minhtrung.nguyen@hotmail.com>
2018-01-06 20:16:47 +0700

    add classify

--- nlp_process.py
+++ nlp_process.py
@@ -1,3 +1,6 @@
 # -*- coding: utf8 -*-
+from underthesea import classify
+
 def nlp_process(text):
-    return (u"Hello there!: " + text);
+    result = classify(text)
+    return ("Hello there! Your text is: {text}. Classify: {result}".format(text=text, result=result));
