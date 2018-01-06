b34c0fe691f48333c63acca8b456272ec1e127a8
Trung <minhtrung.nguyen@hotmail.com>
2018-01-07 00:13:52 +0700

    rescroll npl

--- nlp_process.py
+++ nlp_process.py
@@ -1,6 +1,14 @@
 # -*- coding: utf8 -*-
-from underthesea import classify
+#from underthesea import classify
+#from underthesea import classification
 
 def nlp_process(text):
-    result = classify(text)
-    return ("Hello there! Your text is: {text}. Classify: {result}".format(text=text, result=result));
+    #text = "Chuyên trang chuyển nhượng Transfermarkt mới cập nhật giá trị chuyển nhượng cầu thủ. Trong đó, Kylian Mbappe là cầu thủ tăng giá mạnh nhất, lên tới 90 triệu euro."
+    result = "Classify: "
+    #text_classes = classify(text)
+    #for text_class in text_classes:
+    #    result += text_class + ", "
+    
+    #result += ". Sentiment: " + sentiment(text)
+    #result += ". NER: " + uts.ner(text)
+    return ('Hello there! Your text is: "{text}". NLP: {result}'.format(text=text, result= result));
