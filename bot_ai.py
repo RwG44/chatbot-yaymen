31dbf2af81b8492e75edfd6fbae3a294ff147868
Trung <minhtrung.nguyen@hotmail.com>
2018-01-07 16:11:33 +0700

    install aiml

--- bot_ai.py
+++ bot_ai.py
@@ -5,8 +5,7 @@ kernel = Kernel()
 if os.path.isfile("bot_brain.brn"):
     kernel.bootstrap(brainFile="bot_brain.brn")
 else:
-    kernel.bootstrap(
-        learnFiles="std-startup.xml", commands="load aiml b")
+    kernel.bootstrap(learnFiles="std-startup.xml", commands="load aiml b")
     kernel.saveBrain("bot_brain.brn")
 
 def reply(text, sessionId):
