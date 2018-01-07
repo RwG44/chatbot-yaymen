c5b56744c09b54002090d70f30cb968d6305eed8
Trung <minhtrung.nguyen@hotmail.com>
2018-01-07 17:56:18 +0700

    make real conv

--- bot_ai.py
+++ bot_ai.py
@@ -2,12 +2,26 @@ from aiml import Kernel
 import os
 
 kernel = Kernel()
-if os.path.isfile("bot_brain.brn"):
-    kernel.bootstrap(brainFile="bot_brain_alice.brn")
+
+brain_file = "bot_brain_alice.brn"
+
+if os.path.isfile(brain_file):
+    kernel.bootstrap(brainFile=brain_file)
 else:
     kernel.bootstrap(learnFiles="std-startup.xml", commands="load aiml b")
-    kernel.saveBrain("bot_brain_alice.brn")
+    kernel.setBotPredicate("name", "Yay")
+    kernel.setBotPredicate("city", "Ho Chi Minh City")
+    kernel.setBotPredicate("email", "minhtrung995@gmail.com")
+    kernel.setBotPredicate("master", "Trung Depzai")
+    kernel.setBotPredicate("country", "Vietnam")
+    kernel.setBotPredicate("botmaster", "Creator")
+    kernel.setBotPredicate("nationality", "Vietnamese")
+    kernel.setBotPredicate("age", "22")
+    kernel.setBotPredicate("gender", "female")
+    kernel.setBotPredicate("language", "python")
+
+    kernel.saveBrain(brain_file)    
     #kernel.resetBrain()
-def reply(text, sessionId):
+def reply(text, sessionId = 2215):
     return kernel.respond(text, sessionId)
 
