efeb3436f840fb4b54615bb39c10ef344b35b6ca
Trung <minhtrung.nguyen@hotmail.com>
2018-01-07 16:15:55 +0700

    make real conv

--- nlp_process.py
+++ nlp_process.py
@@ -4,7 +4,7 @@
 from googletrans import Translator
 import bot_ai
 
-def nlp_process(text, sessionId):
+def nlp_process(input_vi, sessionId):
     #text = "Chuyên trang chuyển nhượng Transfermarkt" mới cập nhật giá trị chuyển nhượng cầu thủ. Trong đó, Kylian Mbappe là cầu thủ tăng giá mạnh nhất, lên tới 90 triệu euro."
     #result = "#"
     #text_classes = classify(text)
@@ -14,7 +14,8 @@ def nlp_process(text, sessionId):
     #result += ". Sentiment: " + sentiment(text)
     #result += ". NER: " + uts.ner(text)
     translator = Translator()
-    eng_text = translator.translate(text, dest='en', src='vi').text
-    result = bot_ai.reply(eng_text, sessionId)
-
-    return ('Hello there! Your text is: "{text}". English: {result}'.format(text=text, result= result));
+    input_en = translator.translate(input_vi, dest='en', src='vi').text
+    
+    output_en = bot_ai.reply(input_en, sessionId)
+    output_vi = translator.translate(output_en, dest='vi', src='en').text
+    return output_vi
