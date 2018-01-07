f676a33ee64e5e95ec8bf94ab8ed8777ea82837d
Trung <minhtrung.nguyen@hotmail.com>
2018-01-07 17:48:17 +0700

    update profile aiml

--- bot_ai.py
+++ bot_ai.py
@@ -3,10 +3,11 @@ import os
 
 kernel = Kernel()
 if os.path.isfile("bot_brain.brn"):
-    kernel.bootstrap(brainFile="bot_brain.brn")
+    kernel.bootstrap(brainFile="bot_brain_alice.brn")
 else:
     kernel.bootstrap(learnFiles="std-startup.xml", commands="load aiml b")
-    kernel.saveBrain("bot_brain.brn")
-
+    kernel.saveBrain("bot_brain_alice.brn")
+    #kernel.resetBrain()
 def reply(text, sessionId):
     return kernel.respond(text, sessionId)
+
